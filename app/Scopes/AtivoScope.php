721cbee35351618c62cbf26f5b582c179eb5c92f
Antonio Junior <jr.sistema@gmail.com>
2020-03-13 13:03:29 -0300

    Incluindo arquivo de scopes

---
+++ app/Scopes/AtivoScope.php
@@ -0,0 +1,13 @@
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class AtivoScope implements Scope
+{
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->where('ativo', '=', 0);
+    }
+}
